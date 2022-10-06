84b547d050f904552271a072d4470360df5a858c
wuhuiji2022 <1424129292@qq.com>
2022-10-06 19:46:14 +0800

    webpack vue

---
+++ src/app.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">{{name}}</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: "这是一个webpack+vue项目"
+            };
+        }
+    }
+</script>
+
+<style>
+</style>
